3c8d9edd900b1d6dab5754a3e420ce4ca8e67a66
mikkymeto <m.isimeto@gmail.com>
2022-09-28 13:47:06 +0100

    comment here

--- chaw/settings.py
+++ chaw/settings.py
@@ -81,12 +81,24 @@ WSGI_APPLICATION = 'chaw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chawdb',
+        'USER': 'chawadmin',
+        'PASSWORD': 'admin1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
